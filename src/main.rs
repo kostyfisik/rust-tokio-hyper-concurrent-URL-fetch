e851c25bfcae9217af572fa802e4b5887e92f25d
Konstantin Ladutenko <kostyfisik@gmail.com>
2022-08-30 15:05:18 +0300

    add CLI flags

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use futures::prelude::*;
 use hyper::{body, client::Client};
 use hyper_tls::HttpsConnector;
-use std::{
-    io::{self, Write},
-    iter,
-};
+use std::io::{self, Write};
 use tokio;
 
 use clap::Parser;
@@ -17,8 +14,8 @@ struct Args {
     concurrent_n: usize,
     #[clap(short, long, value_parser, default_value_t = 10)]
     flows_n: usize,
-    #[clap(short, long, value_parser, default_value_t = 10)]
-    nodes_in_flow_n: usize,
+    #[clap(short, long, value_parser, default_value_t = false)]
+    silent: bool,
 }
 
 // const N_CONCURRENT: usize = 80;
@@ -29,6 +26,8 @@ struct Args {
 async fn main() {
     let args = Args::parse();
 
+    println!("use -s to keep silent");
+    println!("Silent mode: {}", args.silent);
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
     // let client = Client::new();
@@ -36,24 +35,40 @@ async fn main() {
     // let uri = "https://jsonplaceholder.typicode.com/photos/12"
     //     .parse()
     //     .unwrap();
-    let uris = create_flow_list("https://jsonplaceholder.typicode.com/photos/12");
+    let uris = create_flow_list(args.flows_n).into_iter();
 
+    // based on https://stackoverflow.com/a/49089303/4280547
     stream::iter(uris)
-        .map(move |uri| client.get(uri))
+        .map(move |uri| execute_flow(uri, &client))
         .buffer_unordered(args.concurrent_n)
         .then(|res| async {
             let res = res.expect("Error making request: {}");
-            println!("Response: {}", res.status());
-
+            if !args.silent {
+                println!("Response: {}", res.status());
+            }
             body::to_bytes(res).await.expect("Error reading body")
         })
         .for_each(|body| async move {
-            io::stdout().write_all(&body).expect("Error writing body");
+            if !args.silent {
+                io::stdout().write_all(&body).expect("Error writing body");
+            }
         })
         .await;
 }
 
-fn create_flow_list(uri_str: &str) -> std::iter::Take<std::iter::Repeat<hyper::Uri>> {
-    let uri = uri_str.parse::<hyper::Uri>().unwrap();
-    iter::repeat(uri).take(2)
+fn create_flow_list(flow_n: usize) -> Vec<hyper::Uri> {
+    let mut vec = Vec::new();
+    for n in 1..flow_n + 1 {
+        let uri_str = "https://jsonplaceholder.typicode.com/photos/".to_string() + &n.to_string();
+        let uri = uri_str.parse::<hyper::Uri>().unwrap();
+        vec.push(uri.clone());
+    }
+    vec
+}
+
+fn execute_flow(
+    uri: hyper::Uri,
+    client: &hyper::Client<HttpsConnector<hyper::client::HttpConnector>>,
+) -> hyper::client::ResponseFuture {
+    client.get(uri)
 }
